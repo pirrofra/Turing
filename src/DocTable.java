5e1c3d23af2c2fb5643b57013e8cf38c08142724
pirrofra <pirro.francesco@gmail.com>
2018-12-26 18:44:04 +0100

    aggiunti commenti DocTable

--- src/DocTable.java
+++ src/DocTable.java
@@ -7,16 +7,37 @@ public class DocTable implements Serializable {
     private ConcurrentHashMap<String,Document> docMap;
     private String docPath;
 
+    /**
+     * Class Constructor with no parameter for ConcurrentHashMap
+     * @param path path where to store all documents' sections
+     */
     public DocTable(String path){
         docMap=new ConcurrentHashMap<>();
         docPath=path;
     }
 
+    /**
+     * Class constructor
+     * @param path path where to store all document's sections
+     * @param initialCapacity hash map initial capacity
+     * @param loadFactor hash map load factor
+     * @param concurrencyLevel number of max concurrent access in docMap
+     */
     public DocTable(String path,int initialCapacity, float loadFactor, int concurrencyLevel){
         docMap=new ConcurrentHashMap<>(initialCapacity,loadFactor,concurrencyLevel);
         docPath=path;
     }
 
+    /**
+     * Method to create a new document
+     * @param name new document name
+     * @param creator user who is creating new document
+     * @param numSections number of sections this document has
+     * @return Operation.FAIL if an I/0 Error occurs
+     *         Operation.Name_not_available if the name is not available
+     *         Operation.OK if successful
+     * @throws IllegalArgumentException if name and/or creator are null or numsections is zero or less
+     */
     public Operation createDocument(String name, String creator, int numSections) throws IllegalArgumentException{
         Document newDoc;
         try{
@@ -29,6 +50,16 @@ public class DocTable implements Serializable {
         else return Operation.NAME_NOT_AVAILABLE;
     }
 
+    /**
+     * Method to invite a new user to edit a document
+     * @param document document the user invited should edit
+     * @param inviter user who sent the invite request
+     * @param invited user who need to be added
+     * @return Operation.Document_Not_Found if the document doesn't exist or the inviter doesn't have permission to add
+     *         Operation.User_already_invited if the user has already been invited
+     *         Operation.Ok if successful
+     * @throws IllegalArgumentException if document, inviter and/or invited are null
+     */
     public Operation invite(String document, String inviter,String invited) throws IllegalArgumentException{
         if(document==null) throw new IllegalArgumentException();
         Document doc=docMap.get(document);
@@ -36,24 +67,77 @@ public class DocTable implements Serializable {
         else return doc.invite(inviter,invited);
     }
 
-    public MessageBuffer Edit(String document,String user,int section) throws IllegalArgumentException,IOException{
+    /**
+     * Method to execute an edit request
+     * @param document document wanted to be edited
+     * @param user user who wants to edit document
+     * @param section section wanted to be edited
+     * @return MessageBuffer containing the result of the operation and the section content if successful
+     *         a generic fail error is sent if an i/o error occurs
+     * @throws IllegalArgumentException if document or user are null or section is not an existing section number
+     */
+    public MessageBuffer edit(String document,String user,int section) throws IllegalArgumentException{
         if(document==null) throw new IllegalArgumentException();
         Document doc=docMap.get(document);
         if(doc==null) return MessageBuffer.createMessageBuffer(Operation.DOCUMENT_NOT_FOUND);
-        else return doc.edit(section,user);
+        else {
+            try{
+                return doc.edit(section,user);
+            }
+            catch (IOException e){
+                return MessageBuffer.createMessageBuffer(Operation.FAIL);
+            }
+        }
     }
 
-    public Operation endEdit(String document,String user,int section, byte[] file) throws IllegalArgumentException,IOException{
-        if(document==null) throw new IllegalArgumentException();
+    /**
+     * Method to execute an end_edit request
+     * @param document document wanted to be updated
+     * @param user user who sent the request
+     * @param section section modified
+     * @param file byte array containing updated section
+     * @return Operation.Document_Not_Found if the user has not been invited to edit document or if document doesn't exist
+     *         Operation.Editing_Not_Request if the user didn't make an edit request first
+     *         Operation.Fail if an I/O error occurs
+     *         Operation.OK if successful
+     * @throws IllegalArgumentException if document or user are null, section is not an existing section number or file is null
+     */
+    public Operation endEdit(String document,String user,int section, byte[] file) throws IllegalArgumentException{
+        if(document==null||file==null) throw new IllegalArgumentException();
         Document doc=docMap.get(document);
         if(doc==null) return Operation.DOCUMENT_NOT_FOUND;
-        else return doc.endEdit(section,user,file);
+        else {
+            try{
+                return doc.endEdit(section,user,file);
+            }
+            catch (IOException e){
+                return Operation.FAIL;
+            }
+        }
     }
 
+    /**
+     * Method to receive the entire document in a MessageBuffer
+     * @param document document wanted to be shown
+     * @param user user who sended the request
+     * @return MessageBuffer containing the result and the entire document if successful
+     *         a generic Fail error is sent if an I/0 error occurs
+     * @throws IllegalArgumentException if document and/or user are null
+     */
+    public MessageBuffer show(String document,String user) throws IllegalArgumentException{
+        if(document==null) throw new IllegalArgumentException();
+        Document doc=docMap.get(document);
+        if(doc==null) return MessageBuffer.createMessageBuffer(Operation.DOCUMENT_NOT_FOUND);
+        else {
+            try{
+                return doc.show(user);
+            }
+            catch (IOException e){
+                return MessageBuffer.createMessageBuffer(Operation.FAIL);
+            }
+        }
+    }
 
-    /* TODO: Edit che restituisce il contenuto del file in un MappedByteBuffer
-    *  TODO: Show che restituisce la concatenazione di tutti i file in un buffer
-    * */
 
 
 }
